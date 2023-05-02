779c95c1a6848914a1e7370009bc53ca8d1af47e
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-03 00:39:54 +0600

    Fav icon added and Route setup completed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Header></Header>
+        <Outlet></Outlet>
+        <Footer></Footer>
+        </>
     );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorElement from "../components/ErrorElement";
+import Home from "../components/Home";
+import ChefRecipes from "../components/ChefRecipes";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Blog from "../components/Blog";
 
 const router = createBrowserRouter([
-    {}
-])
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,//Main layout of the react application
+        errorElement: <ErrorElement></ErrorElement>,
+        children: ([
+            {
+                path:'/',
+                element: <Home></Home>
+            },
+            {
+                path: '/chef_recipes/:id',
+                element: <ChefRecipes></ChefRecipes>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ])
+    }
+]); 
+
+export default router;
